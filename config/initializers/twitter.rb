0ca149dda7025d5a39c216fc75c117c99e7c8eb7
Amit Pandey <amit_iitm@yahoo.com>
2018-06-27 18:44:47 +0530

    Make code rubocop compliant

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,8 @@
-$client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "S3O789gcFFJ2oHkYzMxNkKdQ9"
-  config.consumer_secret     = "DfvCjRTkLIpWugt0n3ZpC0fiEJqob5GDoV47RwieIQJLPpXvtg"
-  config.access_token        = "4494192192-1CqtFIhuSFhMPSLRPKYVgekRnZivLTCRtJoo50T"
-  config.access_token_secret = "F3zunBjePeDQE5OJibrlTOZoCaziKXw45N7nhRCW7KpRw"
+# frozen_string_literal: true
+
+CLIENT = Twitter::REST::Client.new do |config|
+  config.consumer_key        = 'S3O789gcFFJ2oHkYzMxNkKdQ9'
+  config.consumer_secret     = 'DfvCjRTkLIpWugt0n3ZpC0fiEJqob5GDoV47RwieIQJLPpXvtg'
+  config.access_token        = '4494192192-1CqtFIhuSFhMPSLRPKYVgekRnZivLTCRtJoo50T'
+  config.access_token_secret = 'F3zunBjePeDQE5OJibrlTOZoCaziKXw45N7nhRCW7KpRw'
 end
