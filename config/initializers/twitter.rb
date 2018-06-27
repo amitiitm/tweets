b26fa84a3d12bf004e4651345f00c649a60abca3
Amit Pandey <amit_iitm@yahoo.com>
2018-06-27 18:53:00 +0530

    Add Env variable

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 CLIENT = Twitter::REST::Client.new do |config|
-  config.consumer_key        = 'S3O789gcFFJ2oHkYzMxNkKdQ9'
-  config.consumer_secret     = 'DfvCjRTkLIpWugt0n3ZpC0fiEJqob5GDoV47RwieIQJLPpXvtg'
-  config.access_token        = '4494192192-1CqtFIhuSFhMPSLRPKYVgekRnZivLTCRtJoo50T'
-  config.access_token_secret = 'F3zunBjePeDQE5OJibrlTOZoCaziKXw45N7nhRCW7KpRw'
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
